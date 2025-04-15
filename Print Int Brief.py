7dc7864d74ae89d3e6dff7840c876e2d5fd6f6b9
jonathan <jmcnei300@caledonian.ac.uk>
2025-04-15 18:04:16 +0100

    All netmiko project code

--- Print Int Brief.py
+++ Print Int Brief.py
@@ -1,12 +1,22 @@
 from netmiko import ConnectHandler
+import time
+
+# Start the timer
+start_time = time.time()
 
 sshCli = ConnectHandler(
-    device_type='cisco_ios',
-    host='172.16.10.150',
-    port=22,
-    username='admin',
-    password='cisco'
+    device_type='cisco_ios', # The type of device being connected to
+    host='172.16.40.2', # Device IP address
+    port=22, # Port number for SSH
+    username='admin', # SSH username
+    password='cisco123' # SSH password
 )
 
 output = sshCli.send_command("show ip int brief")
 print("show ip int brief:\n{}\n".format(output))
+
+# Stop the timer and calculate the elapsed time
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Execution time: {elapsed_time:.4f} seconds")
